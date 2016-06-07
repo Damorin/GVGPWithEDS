10b61da0037cc3eece15abf0a38f647fe08792cb
Damien <Damorin@protonmail.ch>
2016-06-07 15:47:33 +0100

    Basic Neural Network functioning... incorrectly.

--- src/Test.java
+++ src/Test.java
@@ -23,6 +23,7 @@ public class Test
         String sampleOLMCTSController = "controllers.singlePlayer.sampleOLMCTS.Agent";
         String sampleGAController = "controllers.singlePlayer.sampleGA.Agent";
         String repeatOLETS = "controllers.singlePlayer.repeatOLETS.Agent";
+        String damorinDM = "damorinDM.Agent";
 
         //Available Generators
         String randomLevelGenerator = "levelGenerators.randomLevelGenerator.LevelGenerator";
@@ -56,7 +57,7 @@ public class Test
         int seed = new Random().nextInt();
 
         //Game and level to play
-        int gameIdx = 0;
+        int gameIdx = 4;
         int levelIdx = 0; //level names from 0 to 4 (game_lvlN.txt).
         String game = gamesPath + games[gameIdx] + ".txt";
         String level1 = gamesPath + games[gameIdx] + "_lvl" + levelIdx +".txt";
@@ -65,10 +66,10 @@ public class Test
         String recordActionsFile = null;//"actions_" + games[gameIdx] + "_lvl" + levelIdx + "_" + seed + ".txt"; //where to record the actions executed. null if not to save.
 
         // 1. This starts a game, in a level, played by a human.
-        ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
+//        ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
         
         // 2. This plays a game in a level by the controller.
-//        ArcadeMachine.runOneGame(game, level1, visuals, sampleMCTSController, recordActionsFile, seed, 0);
+        ArcadeMachine.runOneGame(game, level1, visuals, damorinDM, recordActionsFile, seed, 0);
 
         // 3. This replays a game from an action file previously recorded
         //String readActionsFile = recordActionsFile;
--- src/damorinDM/Agent.java
+++ src/damorinDM/Agent.java
@@ -1,18 +1,31 @@
 package damorinDM;
 
+import core.game.Observation;
 import core.game.StateObservation;
 import core.player.AbstractPlayer;
 import ontology.Types.ACTIONS;
 import tools.ElapsedCpuTimer;
 
+import java.util.List;
+
 /**
+ * Basis for attempts to develop an agent for the GVGAI competition using some of the techniques
+ * developed in the Deepmind paper titled "Human Level Control through Deep Reinforcement Learning" in February 2015.
+ *
  * @author Damien Anderson (damorin)
  */
 public class Agent extends AbstractPlayer {
 
+    private NeuralNetwork network;
+
+    public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+        network = new NeuralNetwork();
+    }
+
     @Override
     public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-        return null;
+
+        return network.computeAction(stateObs, elapsedTimer);
     }
 
 }
